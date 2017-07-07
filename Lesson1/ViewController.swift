1dd84f021778960fc1c42785d513ac4361f0ea65
blvvvck <theblvvvck@gmail.com>
2017-07-07 14:11:21 +0300

    Initial Commit

---
+++ Lesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by BLVCK on 07.07.17.
+//  Copyright © 2017 blvvvck production. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
